5a6b61c516140922bbe788500912301d2490b4d3
George B <abductcows@gmail.com>
2024-12-02 18:40:09 +0300

    day 2

---
+++ d2p2.py
@@ -0,0 +1,47 @@
+def get_lines(filename):
+    lines = []
+    with open(filename) as file:
+        for line in file:
+            lines.append(line.rstrip('\n'))
+
+    while lines and not lines[-1]:
+        lines.pop()
+
+    return lines
+
+
+def is_safe_sequence(nums):
+    increasing = nums[0] < nums[1]
+
+    for i in range(len(nums) - 1):
+        test1 = nums[i] == nums[i + 1]
+        test2 = nums[i + 1] > nums[i] and not increasing
+        test3 = nums[i + 1] < nums[i] and increasing
+        test4 = nums[i + 1] > nums[i] + 3 and increasing
+        test5 = nums[i + 1] + 3 < nums[i] and not increasing
+
+        if test1 or test2 or test3 or test4 or test5:
+            return False
+
+    return True
+
+
+def run(filename):
+    lines = get_lines(filename)
+
+    total_safe = 0
+
+    for line in lines:
+        nums = [int(e) for e in line.split(' ')]
+
+        for remove_index in reversed(range(len(nums) + 1)):
+            if is_safe_sequence(nums[:remove_index] + nums[remove_index + 1:]):
+                total_safe += 1
+                break
+
+    print(total_safe)
+
+
+if __name__ == '__main__':
+    run('example.txt')
+    run('input.txt')
