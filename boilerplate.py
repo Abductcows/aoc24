f0986e2baa55657afd6c237a59e11c91cd858cee
George B <abductcows@gmail.com>
2024-12-03 04:09:25 +0300

    add actual boilerplate & add arg checks for input parser

---
+++ boilerplate.py
@@ -0,0 +1,9 @@
+from utils import run_with_file, get_input_for_day
+
+
+def run(lines):
+    pass
+
+if __name__ == '__main__':
+    print(run_with_file('example.txt', run))
+    print(run(get_input_for_day('d')))
--- utils.py
+++ utils.py
@@ -19,10 +19,13 @@ def get_lines(filename):
 
 
 def get_input_for_day(day_with_num):
+    assert re.fullmatch(r'd(\d+)', day_with_num)
     lines = []
     with open('inputs.txt') as file:
-        while not file.readline().startswith(f'{day_with_num}:'):
-            pass
+        while True:
+            line = file.readline()
+            if not line or line.startswith(f'{day_with_num}:'):
+                break
         while True:
             line = file.readline()
             if not line or re.match(r'd(\d+):', line):
